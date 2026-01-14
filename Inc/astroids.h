5d4af3730f67b9648a91a5222ed583a42e9108bb
Alexander Lykke <>
2026-01-14 11:12:25 +0100

    astroider - spawn + bevægelse

---
+++ Inc/astroids.h
@@ -0,0 +1,15 @@
+/*
+ * Astroide.h
+ *
+ *  Created on: 13. jan. 2026
+ *      Author: root
+ */
+
+#ifndef ASTROIDS_H_
+#define ASTROIDS_H_
+void spawn_astroids(void);
+void move_astroids(uint8_t x, uint8_t y);
+
+
+
+#endif /* ASTROIDS_H_ */
